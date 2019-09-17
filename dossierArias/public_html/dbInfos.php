d1de023fb2b0a8d3023d325c63ad0b4bc6487514
donarias <dongorias229@gmail.com>
2019-09-17 12:35:00 +0100

    remove folder donarias

--- dossierArias/public_html/dbInfos.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define('DB_NAME', 'u558406602_gelde');
-	define('DB_USER', 'u558406602_user');
-	define('DB_PASSWORD', '3drueMM1CWta');
-	define('DB_HOST', 'localhost');
-	define('DB_CLIENTS_TABLE', 'clients');
-	define('DB_ADMIN_TABLE', 'admin');
-?>
